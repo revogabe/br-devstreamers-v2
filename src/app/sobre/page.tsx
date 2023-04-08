1a37e60be6ad899c6093c32f6760d7d085cd96b1
Daniel Gabriel <56942108+revogabe@users.noreply.github.com>
2023-04-08 16:39:52 -0300

    Update page.tsx

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -2,7 +2,7 @@ import { AccordionSobre } from '@/components/accordion-sobre'
 
 export default function Sobre() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-24 pt-32">
+    <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-24 pt-32 max-md:px-12">
       <div className="flex flex-col w-full h-max gap-2 items-start justify-center">
         <h1 className="text-3xl font-bold text-gray-100">Sobre</h1>
         <p className="text-gray-300 text-base">
